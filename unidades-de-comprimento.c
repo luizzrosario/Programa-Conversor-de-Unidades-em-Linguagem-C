ab010e33b28bdb44ecc1b9d156bb4a628512d08e
intermediatesystemis <menezesisis814@gmail.com>
2024-12-20 16:31:55 -0300

    Obter entrada do usuario

--- unidades-de-comprimento.c
+++ unidades-de-comprimento.c
@@ -11,5 +11,32 @@ int main() {
 
 // função principal que ficará no arquivo após finalização 
 void unidades_de_comprimento() {
-    printf("Teste\n");
+    char linha [256];
+    int opcao_usuario;
+    printf("\n########## Conversao de unidades de comprimento ##########\n");
+    printf("1. Quilometro (km)\n"
+           "2. Hectometro (hm)\n"
+           "3. Decametro (hm)\n"
+           "4. Metro (m)\n"
+           "5. Decimetro (dm)\n"
+           "6. Centimetro (dm)\n"
+           "7. Milimetro (mm)\n"
+           "8. Voltar ao menu principal\n");   
+    do {
+        printf("Digite o numero corresponde da unidade que sera convertida: ");
+        if (fgets(linha, sizeof(linha), stdin)) {
+            if (1 == sscanf(linha, "%d", &opcao_usuario)) {
+                if (opcao_usuario >= 1 && opcao_usuario <= 7) {
+                    break;
+                } else if (opcao_usuario == 8) {
+                    return;
+                }
+            }
+        }
+       
+        printf("Opcao invalida!\n");
+
+    } while (1);
+         
+
 }
