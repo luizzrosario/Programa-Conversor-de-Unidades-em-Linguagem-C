5ea77cfafc798d1d707c84f15b92195abd599785
intermediatesystemis <menezesisis814@gmail.com>
2024-12-20 15:27:29 -0300

    Criar função principal que será chamada no switch no menu

---
+++ unidades-de-comprimento.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void unidades_de_comprimento();
+
+// função main temporária para testar o código
+int main() {
+    unidades_de_comprimento();
+
+    return 0;
+}
+
+// função principal que ficará no arquivo após finalização 
+void unidades_de_comprimento() {
+    printf("Teste\n");
+}
