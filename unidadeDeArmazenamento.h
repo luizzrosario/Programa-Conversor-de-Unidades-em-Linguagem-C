5dbe0118ba36c61c31c15238e379578e987fb498
Luiz Rosário <lfrasilva.cic@uesc.br>
2024-12-25 23:35:08 -0300

    Revert "Unidade armazenamento"

--- unidadeDeArmazenamento.h
+++ unidadeDeArmazenamento.h
@@ -1,66 +1,107 @@
+// 1 byte = 8 bits
+// 1 kilobyte (KB) = 1024 bytes
+// 1 megabyte (MB) = 1024 kilobytes
+// 1 gigabyte (GB) = 1024 megabytes
+// 1 terabyte (TB) = 1024 gigabytes
+
+
 #include <stdio.h>
 #include <math.h>
 
-#define BIT 1
-#define BYTE 8
-#define KILOBYTE 1024
-#define MEGABYTE (1024 * KILOBYTE)
-#define GIGABYTE (1024 * MEGABYTE)
-#define TERABYTE (1024 * GIGABYTE)
+char *getUnidade(int unidade);
 
-#define CODIGO_BIT 1
-#define CODIGO_BYTE 2
-#define CODIGO_KILOBYTE 3
-#define CODIGO_MEGABYTE 4
-#define CODIGO_GIGABYTE 5
-#define CODIGO_TERABYTE 6
+void unidadeArmazenamento() {
+    
+    double valorUsuario, valor, resultado;
+    int unidade_origem, unidade_destino;
 
-// declaração das funções
-void conversorArmazenamento();
-float conversor_unidade_armazenamento(int unidade_atual, int unidade_escolhida, float valor_atual);
-int codigo_de_unidade_de_medida_valido(int codigo);
+    printf("Digite o valor a ser convertido: ");
+    scanf("%lf", &valorUsuario);
 
-// declaração das funções de conversão
-float converter_bit_para_byte(float valor_bit);
-float converter_bit_para_kilobyte(float valor_bit);
-float converter_bit_para_megabyte(float valor_bit);
-float converter_bit_para_gigabyte(float valor_bit);
-float converter_bit_para_terabyte(float valor_bit);
-float converter_byte_para_bit(float valor_byte);
-float converter_byte_para_kilobyte(float valor_byte);
-float converter_byte_para_megabyte(float valor_byte);
-float converter_byte_para_gigabyte(float valor_byte);
-float converter_byte_para_terabyte(float valor_byte);
-float converter_kilobyte_para_bit(float valor_kilobyte);
-float converter_kilobyte_para_byte(float valor_kilobyte);
-float converter_kilobyte_para_megabyte(float valor_kilobyte);
-float converter_kilobyte_para_gigabyte(float valor_kilobyte);
-float converter_kilobyte_para_terabyte(float valor_kilobyte);
-float converter_megabyte_para_bit(float valor_megabyte);
-float converter_megabyte_para_byte(float valor_megabyte);
-float converter_megabyte_para_kilobyte(float valor_megabyte);
-float converter_megabyte_para_gigabyte(float valor_megabyte);
-float converter_megabyte_para_terabyte(float valor_megabyte);
-float converter_gigabyte_para_bite(float valor_gigabyte);
-float converter_gigabyte_para_byte(float valor_gigabyte);
-float converter_gigabyte_para_kilobyte(float valor_gigabyte);
-float converter_gigabyte_para_megabyte(float valor_gigabyte);
-float converter_gigabyte_para_terabyte(float valor_gigabyte);
-float converter_terabyte_para_bit(float valor_terabyte);
-float converter_terabyte_para_byte(float valor_terabyte);
-float converter_terabyte_para_kilobyte(float valor_terabyte);
-float converter_terabyte_para_gigabyte(float valor_terabyte);
-float converter_terabyte_para_megabyte(float valor_terabyte);
+    valor = valorUsuario;
 
+    printf("Escolha a unidade de origem:\n");
+    printf("1. Bits\n");
+    printf("2. Bytes\n");
+    printf("3. Kilobytes (KB)\n");
+    printf("4. Megabytes (MB)\n");
+    printf("5. Gigabytes (GB)\n");
+    printf("6. Terabytes (TB)\n");
+    scanf("%d", &unidade_origem);                                                   // Logica de capitacao do valor informado pelo usuario
 
+    printf("Escolha a unidade de destino:\n");
 
-void conversorArmazenamento() {
-    int unidade_atual, unidade_escolhida, sair;
-    float valor_atual, valor_convertido;
+    printf("1. Bits\n");
+    printf("2. Bytes\n");
+    printf("3. Kilobytes (KB)\n");
+    printf("4. Megabytes (MB)\n");
+    printf("5. Gigabytes (GB)\n");
+    printf("6. Terabytes (TB)\n");
+    scanf("%d", &unidade_destino);
 
-    do {
-        printf("Digite o valor a ser convertido: ");
-    } while (sair != 1);
+    // Logica para a conversao do valor para a unidade de bytes
+
+    switch (unidade_origem) {
+        case 1: // Bits
+            valor /= 8;
+            break;
+        case 2: // Bytes
+            valor *= 1000;
+            break;
+        case 3: // Kilobytes
+            valor *= 1024;
+            break;
+        case 4: // Megabytes
+            valor *= pow(1024, 2);
+            break;
+        case 5: // Gigabytes
+            valor *= pow(1024, 3); 
+            break;
+        case 6: // Terabytes
+            valor *= pow(1024, 4);
+            break;
+        default:
+            printf("Unidade de origem inválida.\n");
+            return;
+    }
+
+    // Logica para a conversao do valor da unidade de origem para a unidade de destino
+
+    switch (unidade_destino) {
+        case 1: // Bits
+            resultado = valor * 8;
+            break;
+        case 2: // Bytes
+            resultado = valor * 1000;
+            break;
+        case 3: // Kilobytes
+            resultado = valor / 1024;
+            break;
+        case 4: // Megabytes
+            resultado = valor / pow(1024, 2);
+            break;
+        case 5: // Gigabytes
+            resultado = valor / pow(1024, 3);
+            break;
+        case 6: // Terabytes
+            resultado = valor / pow(1024, 4);
+            break;
+        default:
+            printf("Unidade de destino inválida.\n");
+            return;
+    }
+
+    printf("O valor %lf %s corresponde em = %lf %s\n", valorUsuario, getUnidade(unidade_origem), resultado, getUnidade(unidade_destino));             // Imprime o resultado da conversao
+ }
+
+char* getUnidade(int unidade) {
+    switch (unidade) {
+        case 1: return "bits";
+        case 2: return "Bytes";
+        case 3: return "Kilobytes";
+        case 4: return "Megabytes";                             // Funcao para retornar a unidade de armazenamento
+        case 5: return "Gigabytes";
+        case 6: return "Terabytes";
+        default: return "inválida";
+    }
 }
-
-return 0;
