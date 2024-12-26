412596b87951ca4de0cfaf2e134dfa4a0d36a17b
Ladislau Bertoldo <bertoldo.developer@gmail.com>
2024-12-25 21:39:50 -0300

    Corrigindo algumas lacunas no codigo

--- unidadeArmazenamento.h
+++ unidadeArmazenamento.h
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 #define BIT 1
 #define BYTE 8
 #define KILOBYTE 1024
-#define MEGABYTE 1024*2
-#define GIGABYTE 1024*3
-#define TERABYTE 1024*4
+#define MEGABYTE (1024 * KILOBYTE)
+#define GIGABYTE (1024 * MEGABYTE)
+#define TERABYTE (1024 * GIGABYTE)
 
 #define CODIGO_BIT 1
 #define CODIGO_BYTE 2
@@ -30,7 +31,28 @@ float converter_byte_para_kilobyte(float valor_byte);
 float converter_byte_para_megabyte(float valor_byte);
 float converter_byte_para_gigabyte(float valor_byte);
 float converter_byte_para_terabyte(float valor_byte);
-// Continue com as funções para todas as combinações...
+float converter_kilobyte_para_bit(float valor_kilobyte);
+float converter_kilobyte_para_byte(float valor_kilobyte);
+float converter_kilobyte_para_megabyte(float valor_kilobyte);
+float converter_kilobyte_para_gigabyte(float valor_kilobyte);
+float converter_kilobyte_para_terabyte(float valor_kilobyte);
+float converter_megabyte_para_bit(float valor_megabyte);
+float converter_megabyte_para_byte(float valor_megabyte);
+float converter_megabyte_para_kilobyte(float valor_megabyte);
+float converter_megabyte_para_gigabyte(float valor_megabyte);
+float converter_megabyte_para_terabyte(float valor_megabyte);
+float converter_gigabyte_para_bite(float valor_gigabyte);
+float converter_gigabyte_para_byte(float valor_gigabyte);
+float converter_gigabyte_para_kilobyte(float valor_gigabyte);
+float converter_gigabyte_para_megabyte(float valor_gigabyte);
+float converter_gigabyte_para_terabyte(float valor_gigabyte);
+float converter_terabyte_para_bit(float valor_terabyte);
+float converter_terabyte_para_byte(float valor_terabyte);
+float converter_terabyte_para_kilobyte(float valor_terabyte);
+float converter_terabyte_para_gigabyte(float valor_terabyte);
+float converter_terabyte_para_megabyte(float valor_terabyte);
+
+
 
 void conversorArmazenamento() {
     int unidade_atual, unidade_escolhida, sair;
